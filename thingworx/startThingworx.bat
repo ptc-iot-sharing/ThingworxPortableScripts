d2d6c4f32ed7f60e597b4f3a20a4a82a2e8b711b
Petrisor Lacatus <placatus@ptc.com>
2017-06-26 11:40:19 +0300

    Nicer config.properties with more options More logging in startup files Removed shutdown port

--- thingworx/startThingworx.bat
+++ thingworx/startThingworx.bat
@@ -1,15 +1,20 @@
 @echo off
 SETLOCAL enabledelayedexpansion
 
-rem configure any other sys variables you would have in setenv.bat here
+echo ==========================================
+echo Starting up portable thingworx instance...
+echo ==========================================
+echo Reading config.properties...
+echo ==========================================
 
-rem read the propeties file
+rem read the properties file
 for /F "eol=# delims== tokens=1,*" %%a in (config.properties) do (
-    if NOT "%%a"=="" if NOT "%%b"=="" set config.%%a=%%b
+    set aux=%%a
+    if NOT "%%a"=="" if NOT "%%b"=="" set config_!aux:.=_!=%%b
 )
 
-IF DEFINED config.JAVA_HOME (
-    SET JAVA_HOME=%config.JAVA_HOME%
+IF DEFINED config_JAVA_HOME (
+    SET JAVA_HOME=%config_JAVA_HOME%
 )
 IF NOT DEFINED JAVA_HOME ( 
     rem attempt to get the JAVA_HOME from registry
@@ -38,24 +43,31 @@ IF NOT DEFINED JAVA_HOME (
         set JAVA_HOME=%%b
     )
 )
+
 set JAVA_HOME
 rem set the terminal title
-TITLE %config.instanceName%
-rem the platform_settigns.json is in teh same folder 
+TITLE %config_instanceName%
+rem the platform_settigns.json is in the same folder 
 SET THINGWORX_PLATFORM_SETTINGS=%cd%
-
+echo ==========================================
+echo ==========================================
+echo Finished parsing config.properties. Starting tomcat on http port %config_http_port% and https port %config_https_port%
 pushd apache-tomcat
-IF /I "%config.enableDebugging%"=="true" (
+IF /I "%config_debugging_enable%"=="true" (
     rem only enable debugging if needed
-    SET debugCommand=-agentlib:jdwp=transport=dt_socket,address=%config.debuggingPort%,server=y,suspend=n
+    SET debugCommand=-agentlib:jdwp=transport=dt_socket,address=%config_debugging_port%,server=y,suspend=n
+    echo Debugging is enabled and starting on %config_debugging_port%.
 )
-set CATALINA_OPTS=-Dserver -Dfile.encoding=UTF-8 -Djava.library.path=webapps/Thingworx/WEB-INF/extensions  -Dd64 -XX:+UseNUMA -XX:+UseConcMarkSweepGC -XX:PermSize=128m -XX:MaxPermSize=2048m -Xms1024M -Dport.http=%config.httpPort% -Dport.https=%config.httpsPort% -Dshutdownport.http=%config.shutdownPort% %debugCommand%
+echo ==========================================
+echo ==========================================
+set CATALINA_OPTS=-server -Dfile.encoding=UTF-8 -Djava.library.path=webapps/Thingworx/WEB-INF/extensions -d64 -XX:+UseG1GC -Xms%config_config_minMemory% -Dport.http=%config_http_port% -Dport.https=%config_https_port% -Dhttps.keystorePassword=%config_https_keystorePassword% %debugCommand% %config_config_additionalParams% 
 SET CATALINA_HOME=%CURRENT_DIR%
 call bin\catalina.bat run %CATALINA_OPTS%
 
 popd
 
 rem cleanup namespace config.
-for /F "tokens=1 delims==" %%v in ('set config. 2^>nul') do (
-    set %%v=
+for /F "tokens=1 delims==" %%v in ('set config_ 2^>nul') do (
+   set %%v=
 )
+pause
--- thingworx/startThingworx.sh
+++ thingworx/startThingworx.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-#configure here all the properties you would otherwise set in setenv.sh
+## Bash script for starting thingworx
+## Should be compatible with Linux, as well as OSX
+## It will also run on windows under MinGW, but has been less tested there
+
+RED='\033[0;31m'
+WHITE_BG='\033[47m'
+NO_BG='\033[0m'
+NC='\033[0m' # No Color
+
+echo -e "${RED}Starting up portable thingworx instance...${NC}"
+echo -e "${RED}Reading ${WHITE_BG}config.properties${NO_BG}${RED}...${NC}"
 
 #read the properties file
 file="./config.properties"
@@ -19,21 +29,25 @@ then
   PROMPT_COMMAND="echo -ne ${config_instanceName}"
   export THINGWORX_PLATFORM_SETTINGS=$(pwd)
   export CATALINA_PID="$(pwd)/running.pid"
-  pushd apache-tomcat
+  pushd apache-tomcat > /dev/null
   
   #make sure all the files in the bin folder are executable
   chmod +x bin/*.sh
 
-  if [ "${config_enableDebugging}" == "true" ] ; then
+  echo -e "${RED}Finished parsing ${WHITE_BG}config.properties${NO_BG}${RED}. Starting tomcat on http port ${WHITE_BG}${config_http_port}${NO_BG}${RED} and https port ${WHITE_BG}${config_https_port}${NO_BG}${NC}"
+
+  if [ "${config_debugging_enable}" == "true" ] ; then
     # only enable debugging if needed
-    debugCommand="-agentlib:jdwp=transport=dt_socket,address=${config_debuggingPort},server=y,suspend=n"
+    debugCommand="-agentlib:jdwp=transport=dt_socket,address=${config_debugging_port},server=y,suspend=n"
+    echo -e "${RED}Debugging is enabled and starting on ${WHITE_BG}${config_debugging_port}${NO_BG}${NC}" 
  fi
-export CATALINA_OPTS="-Dserver -Dfile.encoding=UTF-8 -Djava.library.path=webapps/Thingworx/WEB-INF/extensions -Dd64 -XX:+UseNUMA -XX:+UseConcMarkSweepGC -XX:PermSize=128m -XX:MaxPermSize=2048m -Xms512M -Dport.http=${config_httpPort} -Dport.https=${config_httpsPort} -Dshutdownport.http=${config_shutdownPort} ${debugCommand}"
+
+export CATALINA_OPTS="-server -Dfile.encoding=UTF-8 -Djava.library.path=webapps/Thingworx/WEB-INF/extensions -d64 -XX:+UseG1GC -Xms${config_config_minMemory} -Dport.http=${config_http_port} -Dport.https=${config_https_port} -Dhttps.keystorePassword=${config_https_keystorePassword} ${debugCommand} ${config_config_additionalParams} "
 export CATALINA_HOME=$(pwd)
 
 source bin/catalina.sh run $CATALINA_OPTS
 
-popd
+popd > /dev/null
 
 else
   echo "$file not found."
